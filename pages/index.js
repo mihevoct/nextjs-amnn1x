22d94584682b6fe648fe0b211f76daec55ac1bdf
mihevoct <86762610+mihevoct@users.noreply.github.com>
2022-09-22 12:19:13 +0300

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { useState } from 'react';
 
 export default function Home() {
   const [count, setCount] = useState(0);
