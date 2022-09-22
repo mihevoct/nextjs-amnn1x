7b399253ad3bfe79d1e40e57cd7cc6606141a05b
mihevoct <86762610+mihevoct@users.noreply.github.com>
2022-09-22 12:18:31 +0300

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className={styles.container}>
       <button>click Me</button>
