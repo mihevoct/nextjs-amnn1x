180bd365045938ced0401e4379aa02e84b310fc9
mihevoct <86762610+mihevoct@users.noreply.github.com>
2022-09-22 12:21:54 +0300

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      <button onClick={increment}>count is: {count}</button>
+      <button onClick={increment()}>count is: {count}</button>
     </div>
   );
 }
